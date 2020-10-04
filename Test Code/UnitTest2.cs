b404ec7934802ed9393ce42199d26210aa82331a
harun470 <70342700+harun470@users.noreply.github.com>
2020-10-04 12:17:29 +0600

    Add files via upload

---
+++ Test Code/UnitTest2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartsTests
+    {
+        [TestMethod]
+        public void can_add_new_lines()
+        {
+            //add the product for the first time
+            Cart target = new Cart();
+            target.AddItem(new Product{ ProductID=1, Name="p1", Category="c1", Price=10}, 10);
+
+            CartLine[] actual = ((List<CartLine>)target.Lines).ToArray();
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreEqual(1, actual[0].Product.ProductID);
+        }
+
+        [TestMethod]
+        public void can_add_quantity_to_the_existing_lines()
+        {
+            Cart target = new Cart();
+            target.AddItem(new Product{ ProductID=1, Name="p1", Category="c1", Price=10}, 10);
+            target.AddItem(new Product { ProductID = 1, Name = "p1", Category = "c1", Price = 10 }, 10);
+
+            CartLine[] actual = ((List<CartLine>)target.Lines).ToArray();
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreEqual(20, actual[0].Quantity);
+        }
+
+        [TestMethod]
+        public void remove_line()
+        {
+            Cart target = new Cart();
+            target.AddItem(new Product{ ProductID=1, Name="p1", Category="c1", Price=10}, 10);
+            target.AddItem(new Product{ ProductID=2, Name="p2", Category="c1", Price=10}, 10);
+            target.AddItem(new Product{ ProductID=3, Name="p3", Category="c1", Price=10}, 10);
+            target.RemoveLine(new Product { ProductID = 3, Name = "p3", Category = "c1", Price = 10 });
+            CartLine[] actual = ((List<CartLine>)target.Lines).ToArray();
+            Assert.AreEqual(2, actual.Length);
+        }
+
+        [TestMethod]
+        public void calculate_cart_total()
+        {
+            Cart target = new Cart();
+            target.AddItem(new Product{ ProductID=1, Name="p1", Category="c1", Price=10}, 10);
+            target.AddItem(new Product{ ProductID=2, Name="p2", Category="c1", Price=20}, 20);
+            target.AddItem(new Product{ ProductID=3, Name="p3", Category="c1", Price=30}, 30);
+            decimal actual = target.ComputeTotalValue();
+            Assert.AreEqual(1400M, actual);
+        }
+
+        [TestMethod]
+        public void can_clear_contents()
+        {
+            Cart target = new Cart();
+            target.AddItem(new Product{ ProductID=1, Name="p1", Category="c1", Price=10}, 10);
+            target.AddItem(new Product{ ProductID=2, Name="p2", Category="c1", Price=10}, 10);
+            target.AddItem(new Product{ ProductID=3, Name="p3", Category="c1", Price=10}, 10);
+            target.Clear();
+           
+            Assert.AreEqual(0, target.Lines.Count());
+        }
+    }
+}
---
+++ Test Code/UnitTest3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void index_contains_all_products()
+        {
+            Mock<IProductRepository> _m = new Mock<IProductRepository>();
+            _m.Setup(m => m.Products).Returns(
+
+                                       new List<Product>
+                                                   { new Product(){ProductID=1,Name="Product1"},
+                                                     new Product(){ProductID=1,Name="Product2"},
+                                                     new Product(){ProductID=1,Name="Product3"},
+                                                   }.AsQueryable()
+           );
+            AdminController target = new AdminController(_m.Object);
+
+            IQueryable<Product> p = (IQueryable<Product>)target.Index().Model;
+            Product[] _p = p.ToArray();
+
+            Assert.AreEqual(3, _p.Length);
+            Assert.AreEqual("Product1", _p[0].Name);
+            Assert.AreEqual("Product2", _p[1].Name);
+            Assert.AreEqual("Product3", _p[2].Name);
+        }
+    }
+}
